51c0401fef05cc5aa1ad1cc3c39d88f67fa0e81d
LevZolotykh <levzolotykh@mail.ru>
2024-10-15 09:06:15 +0300

    Header adaptive + ref logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,18 @@
-import React, { useState } from "react";
 import classes from "./UI/Header.module.css";
 
 const Header = () => {
-  const [IsOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!IsOpen);
-  };
   return (
     <header className={classes.header} id="QHeader">
-      <div className={classes.logo}>
-        <img src="/images/Logo.svg" alt="Logo" />
-        <span>Kvizik</span>
-      </div>
-      <nav className={`${classes.navLinks} ${IsOpen ? "open" : ""}`}>
+      <a href="#Qheader">
+        <div className={classes.logo}>
+          <img src="/images/Logo.svg" alt="Logo" />
+          <span>Kvizik</span>
+        </div>
+      </a>
+      <nav className={classes.navLinks}>
         <a href="#HowWorks">Как работает</a>
         <a href="#ForWhom">Для кого</a>
       </nav>
-      <div
-        className={`${classes.burger} ${IsOpen ? "open" : ""}`}
-        role="button"
-        tabIndex="0"
-        onClick={toggleMenu}
-      >
-        <div className={classes.line}></div>
-        <div className={classes.line}></div>
-        <div className={classes.line}></div>
-      </div>
     </header>
   );
 };
